6d48a2e3f8e5c022baa208a8217a297f844c3a69
frankharerimana <fharerimana@objectivity.co.uk>
2023-09-25 09:42:37 +0200

    configures automapper

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using AfsTranslator.src.Repositories.Dtos;
+using AfsTranslator.src.Repositories.Models;
+
+namespace AfsTranslator
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<TranslationLog, TranslationLogDto>().ReverseMap();
+
+        }
+    }
+}
+
