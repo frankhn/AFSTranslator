790757820d53aa2f36d4de73f8ec7048711c96df
frankharerimana <fharerimana@objectivity.co.uk>
2023-09-25 09:43:33 +0200

    update views with translation list

---
+++ Views/Models/LogTranslationText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AfsTranslator.src.Repositories.Dtos;
+using AfsTranslator.src.Repositories.Models;
+
+namespace AfsTranslator.Views.Models
+{
+	public class LogTranslationText
+	{
+		public List<TranslationLogDto>? translationLog { get; set; }
+
+		public Translation? translation { get; set; }
+
+	}
+}
+
