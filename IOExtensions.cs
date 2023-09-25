5648e1df314484ca29c67606b25b7c267311d4a8
frankharerimana <fharerimana@objectivity.co.uk>
2023-09-25 09:42:55 +0200

    configures dependency injections

---
+++ IOExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AfsTranslator.src.Services.Interfaces.IServices;
+using AfsTranslator.src.Services.Interfaces.IRepository;
+using AfsTranslator.src.Services;
+using AfsTranslator.src.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AfsTranslator
+{
+	public static class IOExtensions
+	{
+		public static void RegisterServices(this IServiceCollection services)
+		{
+			services.AddTransient<ITranslationLogService, TranslationLogService>();
+			services.AddTransient<ITranslationService, TranslationService>();
+        }
+
+		public static void RegisterRepository(this IServiceCollection services)
+		{
+			services.AddTransient<ITranslationLogRepository, TranslationLogRepository>();
+			services.AddTransient<ITranslationRepository, TranslationRepository>();
+        }
+	}
+}
+
